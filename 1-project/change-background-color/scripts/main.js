53d1ef14f58d2c6e5b74b984c7413e110d2a67a6
DatTram <62746171+DatTram@users.noreply.github.com>
2020-12-25 11:54:24 +0200

    Update main.js

--- 1-project/change-background-color/scripts/main.js
+++ 1-project/change-background-color/scripts/main.js
@@ -1,1 +1,10 @@
+function changeColor() {
+    const first = (Math.floor(Math.random() * 255)+1);
+    const second = (Math.floor(Math.random() * 255)+1);
+    const third  = (Math.floor(Math.random() * 255)+1);
+    const color = `rgb(${first}, ${second}, ${third})`;
+
+    document.querySelector("body").style.backgroundColor = color;
+}   
+
 
