192e41901050a870d75ee8c5cb1dc40392a7c8d5
DatTram <62746171+DatTram@users.noreply.github.com>
2020-12-26 11:23:54 +0200

    Add files via upload

--- 1-project/change-background-color(hex)/scripts/main.js
+++ 1-project/change-background-color(hex)/scripts/main.js
@@ -1,1 +1,15 @@
+const hexcode = ["0","1","2","3","4",
+                 "5","6","7","8","9",
+                 "a","b","c", "d", "e","f"];
 
+function changeColor() {
+    let newCode = "#";
+    for (let i = 0; i <= 5; i++) {
+        newCode += hexcode[Math.floor(Math.random()*hexcode.length)];
+    }
+
+    document.querySelector("body").style.backgroundColor = newCode;
+    document.querySelector("h1").textContent = `HEX COLOR: ${newCode}`
+}
+
+changeColor();
