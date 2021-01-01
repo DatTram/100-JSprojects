e98788f5a9084a2cce03552d9b71eb100832842f
DatTram <62746171+DatTram@users.noreply.github.com>
2021-01-01 16:50:35 +0200

    Add files via upload

--- 2-project/random-quote-generator/scripts/main.js
+++ 2-project/random-quote-generator/scripts/main.js
@@ -1,1 +1,30 @@
+function generate() {
+    fetch("https://quotes15.p.rapidapi.com/quotes/random/", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "bfea68e228mshc2194e5071128eep11229cjsn71f3ed733672",
+		"x-rapidapi-host": "quotes15.p.rapidapi.com"
+	}
+    })
+    .then(response => {
+        return response.json()
+    })
+    .then(json => {
+        display(json);
+    })
+    .catch(err => {
+        console.error(err);
+    });
+}
+
+function display(quote) {
+    document.querySelector("h3").textContent = "";
+    let para = document.querySelector("p");
+    let link = document.createElement("a");
+
+    link.href = quote["originator"]["url"];
+    link.textContent = quote["originator"]["name"]
+    para.textContent = quote["content"];
+    document.querySelector("h3").appendChild(link);
+}
 
